86c12340c7324fbb9164bb0907e874dd8b68fc58
MaxPunzMaxPunz <162301249+MaxPunzMaxPunz@users.noreply.github.com>
2025-07-21 12:52:02 +0200

    Update middlewares.js

--- exams/config/middlewares.js
+++ exams/config/middlewares.js
@@ -1,12 +1,21 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://exams.integriert-studieren.jku.at"], // oder '*', wenn du alles zulassen willst
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      headers: ["Content-Type", "Authorization"],
+      credentials: true,
+    },
+  },
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
